153c0b94190277afe2ec3f0ff6088b9fe122bbb0
Skittlq <skittlq@gmail.com>
2025-07-04 05:09:42 +1000

    ore generation added basic functionality of mod is complete, it is playable. but i will start adding other things such as cool special abilties and blocks and stuff.

--- src/main/java/com/skittlq/endernium/datagen/DataGenerators.java
+++ src/main/java/com/skittlq/endernium/datagen/DataGenerators.java
@@ -4,15 +4,10 @@ import com.skittlq.endernium.Endernium;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @EventBusSubscriber(modid = Endernium.MODID)
@@ -24,6 +19,7 @@ public class DataGenerators {
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
         generator.addProvider(true, new ModModelProvider(packOutput));
+        generator.addProvider(true, new ModDatapackProvider(packOutput, lookupProvider));
 
     }
 
@@ -34,5 +30,6 @@ public class DataGenerators {
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
         generator.addProvider(true, new ModModelProvider(packOutput));
+        generator.addProvider(true, new ModDatapackProvider(packOutput, lookupProvider));
     }
 }
---
+++ src/main/java/com/skittlq/endernium/worldgen/ModConfiguredFeatures.java
@@ -0,0 +1,37 @@
+package com.skittlq.endernium.worldgen;
+
+import com.skittlq.endernium.Endernium;
+import com.skittlq.endernium.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
+
+public class ModConfiguredFeatures {
+    public static final ResourceKey<ConfiguredFeature<?, ?>> ENDERNIUM_ORE_KEY = registerKey("endernium_ore");
+
+    public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
+        RuleTest endReplaceables = new BlockMatchTest(Blocks.END_STONE);
+
+        register(context, ENDERNIUM_ORE_KEY, Feature.ORE, new OreConfiguration(endReplaceables,
+                ModBlocks.ENDERNIUM_ORE.get().defaultBlockState(), 9));
+    }
+
+    public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, ResourceLocation.fromNamespaceAndPath(Endernium.MODID, name));
+    }
+
+    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstrapContext<ConfiguredFeature<?, ?>> context,
+                                                                                          ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
+        context.register(key, new ConfiguredFeature<>(feature, configuration));
+    }
+}
