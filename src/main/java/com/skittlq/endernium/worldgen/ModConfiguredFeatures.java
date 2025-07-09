205f67e84fa4ba043e1566e264321d9daf963e47
Skittlq <skittlq@gmail.com>
2025-07-10 00:01:30 +1000

    updated to 1.21.7, moddev 2.0.99, neoforge 21.7.13-beta

--- src/main/java/com/skittlq/endernium/worldgen/ModConfiguredFeatures.java
+++ src/main/java/com/skittlq/endernium/worldgen/ModConfiguredFeatures.java
@@ -6,7 +6,6 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
@@ -14,7 +13,6 @@ import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfigur
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
-import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 
 public class ModConfiguredFeatures {
     public static final ResourceKey<ConfiguredFeature<?, ?>> ENDERNIUM_ORE_KEY = registerKey("endernium_ore");
@@ -23,7 +21,7 @@ public class ModConfiguredFeatures {
         RuleTest endReplaceables = new BlockMatchTest(Blocks.END_STONE);
 
         register(context, ENDERNIUM_ORE_KEY, Feature.ORE, new OreConfiguration(endReplaceables,
-                ModBlocks.ENDERNIUM_ORE.get().defaultBlockState(), 9));
+                ModBlocks.ENDERNIUM_ORE.get().defaultBlockState(), 4));
     }
 
     public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
